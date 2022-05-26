2b8b3904f38d35d2daf70b224654b72c42f3045c
Kevin Andries <kevin.andries@yahoo.fr>
2022-05-26 16:10:31 +0300

    Update compiler version

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const HDWallerProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const { abi, evm } = require("./compile");
 
 const provider = new HDWallerProvider(
 	"someone task castle early chair fee drum wave attack utility else essay",
@@ -14,13 +14,16 @@ const web3 = new Web3(provider);
 
 	console.log("Attempting to deploy from account", accounts[0]);
 
-	const inbox = await new web3.eth.Contract(JSON.parse(interface))
+	const inbox = await new web3.eth.Contract(abi)
 		.deploy({
-			data: bytecode,
+			data: evm.bytecode.object,
 			arguments: ["Hi there!"],
 		})
 		.send({ gas: "1000000", from: accounts[0] });
 
+	// Log newly deployed contract address
 	console.log("Contract address", inbox.options.address);
-	process.exit(1);
+
+	// Strop provider
+	provider.engine.stop();
 })();
