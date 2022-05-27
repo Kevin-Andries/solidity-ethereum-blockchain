8264a3bf3a7ee594c540d20e69cf3291f5801456
Kevin Andries <kevin.andries@yahoo.fr>
2022-05-27 09:34:22 +0300

    Fix typo

--- deploy.js
+++ deploy.js
@@ -1,8 +1,8 @@
-const HDWallerProvider = require("@truffle/hdwallet-provider");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
 const { abi, evm } = require("./compile");
 
-const provider = new HDWallerProvider(
+const provider = new HDWalletProvider(
 	"someone task castle early chair fee drum wave attack utility else essay",
 	"https://rinkeby.infura.io/v3/fa0749f52012478e82b6970f80e5e8fb"
 );
@@ -11,7 +11,6 @@ const web3 = new Web3(provider);
 
 (async () => {
 	const accounts = await web3.eth.getAccounts();
-
 	console.log("Attempting to deploy from account", accounts[0]);
 
 	const inbox = await new web3.eth.Contract(abi)
